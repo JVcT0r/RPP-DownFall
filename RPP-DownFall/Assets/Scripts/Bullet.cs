ceb0894fbf0f71e597cd81501c787a96b9257acb
Darkvergil <145566193+Darkfighter4@users.noreply.github.com>
2025-07-05 01:32:42 -0300

    Tiro acabado

---
+++ RPP-DownFall/Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage = 1; 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Inimigo"))
+        {
+            EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+            Destroy(gameObject);
+        }
+    }
+}
+
+
