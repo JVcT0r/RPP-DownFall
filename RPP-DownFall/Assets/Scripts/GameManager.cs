2b71ef666021fc478919f2d0cf406265f4efc21b
João Victor M. <145566469+JVcT0r@users.noreply.github.com>
2025-10-13 22:29:41 -0300

    Update GameManager.cs

--- RPP-DownFall/Assets/Scripts/GameManager.cs
+++ RPP-DownFall/Assets/Scripts/GameManager.cs
@@ -57,8 +57,8 @@ public class GameManager : MonoBehaviour
     public void UnPause()
     {
         Time.timeScale = 1f;
-        paused = false;
         UnloadSceneAsync("UI_PauseScreen");
+        paused = false;
     }
     
 
