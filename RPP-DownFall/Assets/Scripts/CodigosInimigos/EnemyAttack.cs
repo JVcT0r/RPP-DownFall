b5bf4afc9d6e9d695d9ac7c75f286c585c5420e6
João Victor M. <145566469+JVcT0r@users.noreply.github.com>
2025-11-15 21:35:10 -0300

    audio attack inimigo

--- RPP-DownFall/Assets/Scripts/CodigosInimigos/EnemyAttack.cs
+++ RPP-DownFall/Assets/Scripts/CodigosInimigos/EnemyAttack.cs
@@ -5,9 +5,14 @@ public class EnemyAttack : MonoBehaviour
     public GameObject damageArea;
     private PolygonCollider2D _collider2D;
     
+    [Header("Audios")]
+    private AudioSource _audioSource;
+    public AudioClip attack_Sfx;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         damageArea = transform.Find("DamageArea").gameObject;
         damageArea.SetActive(false);
         _collider2D = damageArea.GetComponent<PolygonCollider2D>();
@@ -25,6 +30,8 @@ public class EnemyAttack : MonoBehaviour
     void EnableDamageArea()
     {
         damageArea.SetActive(true);
+        _audioSource.pitch = Random.Range(1f, 1.5f);
+        _audioSource.PlayOneShot(attack_Sfx);
         //_collider2D.enabled = true;
         
     }
